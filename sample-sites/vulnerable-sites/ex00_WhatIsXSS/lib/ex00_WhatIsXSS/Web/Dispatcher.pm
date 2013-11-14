9458509d90bb4735708e2028a295d281f8e3f724
Sho Nakatani <lay.sakura@gmail.com>
2013-11-14 15:50:46 +0900

    updates

--- sample-sites/vulnerable-sites/ex00_WhatIsXSS/lib/ex00_WhatIsXSS/Web/Dispatcher.pm
+++ sample-sites/vulnerable-sites/ex00_WhatIsXSS/lib/ex00_WhatIsXSS/Web/Dispatcher.pm
@@ -10,6 +10,33 @@ any '/' => sub {
     });
 };
 
+post '/confirm' => sub {
+    my $c = shift;
+    my $uid  = $c->req->param('uid');
+    my $pass = $c->req->param('pass');
+    
+    my $sid = "";
+    eval {
+        # my $sid = uid, pass をDBから引いて，OKならcurrent_sidカラムを設定しつつそのsidを返す
+        $sid = "atode_nantoka_uid_${uid}_kara";
+    };
+    if($@) {
+        # login failed
+        return $c->redirect("/");
+    }
+    return $c->redirect("/login?sid=$sid");
+};
+
+get '/login' => sub {
+    my $c = shift;
+    my $sid = $c->req->param('sid');
+    # my $uid = sidからuidをDBでひく
+    my $uid = "sid_${sid}_kara_db_hiku";
+    return $c->render('login.tx', {
+        uid => $uid,
+    });
+};
+
 get '/status' => sub {
     my $c = shift;
     my $uid  = $c->req->param('uid');
